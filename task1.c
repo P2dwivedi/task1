843bac8cc02cbc8033b6ec57bfb77a76be874943
P2dwivedi <108986937+P2dwivedi@users.noreply.github.com>
2022-08-04 23:53:41 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a=12,b=3;
+ int sum=0,sub=0,divide=0,modulus=0;
+ sum =a+b;
+ sub = a-b;
+ divide = a/b;
+ modulus =a%b;
+ printf("\n sum=%d,\n sub=%d,\n divide=%d,\n modulus=%d", sum,sub,divide,modulus);
+ return 0;  
+}
